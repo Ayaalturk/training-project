7d61bed54b1bd123fb9e636d4117d5791f819c29
Aya Alturk <ayaalturk99@gmail.com>
2021-03-13 17:32:23 +0200

    task

--- src/app/landing/posts/posts.component.ts
+++ src/app/landing/posts/posts.component.ts
@@ -6,23 +6,19 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
+
 export class PostsComponent implements OnInit {
- posts: any;
+  
+  posts: any;
 
   constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
     this.http.get("https://jsonplaceholder.typicode.com/posts")
       .subscribe(
-        data=>{ this.displayPost(data)
-        },
-        error=>{
-          console.log(error)
-        }
+         data=>{ this.posts=data},
+         error=>{console.log(error)}
         );
-    
   }
- displayPost(data: Object){
-   this.posts=data;
- }
+
 }
--- src/app/pipes/add.pipe.ts
+++ src/app/pipes/add.pipe.ts
@@ -6,12 +6,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class AddPipe implements PipeTransform {
 
   transform(value: unknown, ...args: unknown[]) {
-  if(!value)
-    return null;
-    else
-    return value +"Shoe-NO"
+   if(!value)
+      return null;
 
-  
+   else
+
+     return value +"Shoe-NO"
   }
 
 }
